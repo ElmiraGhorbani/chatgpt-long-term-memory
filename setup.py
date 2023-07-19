55fe3c61eb7255ad2365608116ebfc9d6275db89
ElmiraGhorbani <elmira.ghorbani96@gmail.com>
2023-07-19 16:03:20 +0330

    update setup requirements

--- setup.py
+++ setup.py
@@ -15,7 +15,9 @@ setup(
     description='Empower your projects with a large knowledge base and adaptive memory using Chatgpt Long Term Memory.',
     long_description='The Chatgpt Long Term Memory package is designed to empower your projects by accommodating a large number of simultaneous users while leveraging an extensive knowledge base and adaptive memory. This capability is made possible through the integration of cutting-edge technologies, including GPT (OpenAI), llama vector index, and Redis datastore.',    packages=find_packages(),
     install_requires=[
-        'redis-chatgpt>=0.1.2'
+        'redis-chatgpt>=0.1.2',
+        'llama-index>=0.7.9',
+        'openai>=0.27.8'
     ],
     project_urls=project_urls,
     long_description=long_description,
