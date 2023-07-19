7c5e5b09860e09e6b023abbaceeedfb90e058797
ElmiraGhorbani <elmira.ghorbani96@gmail.com>
2023-07-19 15:56:40 +0330

    add __init__.py

---
+++ chatgpt_long_term_memory/__init__.py
@@ -0,0 +1,8 @@
+from chatgpt_long_term_memory.llama_index_helpers import DocIndexer, Retrievers
+from chatgpt_long_term_memory.memory import ChatMemory
+
+__all__ = [
+    "DocIndexer",
+    "Retrievers",
+    "ChatMemory"
+]
