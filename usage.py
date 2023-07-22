bdfc78388628c8faad6e4e84cddd164973cbaa9e
ElmiraGhorbani <elmira.ghorbani96@gmail.com>
2023-07-22 17:26:39 +0330

    update usage sample

--- usage.py
+++ usage.py
@@ -12,19 +12,14 @@ def get_project_root():
     # Return the parent directory (project root)
     return os.path.dirname(os.path.dirname(current_path))
 
-
+# root path that contains resource data
 root_path = get_project_root()
-doc_indexer_config = IndexConfig(root_path=root_path,
-                                 knowledge_base=True,  # Set this to True or False based on whether the user has a personal knowledge base.
-                                 model_name="gpt-3.5-turbo",
-                                 temperature=0,
-                                 context_window=4096,
-                                 num_outputs=700,
-                                 max_chunk_overlap=0.5,
-                                 chunk_size_limit=600)
+doc_indexer_config = IndexConfig(root_path=f"{root_path}/chatgpt-long-term-memory/example/chatbot", knowledge_base=False)
 retrievers_config = RetrieversConfig()
-chat_memory_config = ChatMemoryConfig()
-chatgpt_client = ChatGPTClient(doc_indexer_config=IndexConfig,
-                               retrievers_config=retrievers_config, chat_memory_config=chat_memory_config)
+chat_memory_config = ChatMemoryConfig(
+    redis_host="172.0.0.22",
+    redis_port=6379
+)
+chatgpt_client = ChatGPTClient(doc_indexer_config=doc_indexer_config, retrievers_config=retrievers_config, chat_memory_config=chat_memory_config)
 
-print(chatgpt_client.converse('hi', '1'))
+print(chatgpt_client.converse("hi", '1'))
