19bc154a1b543a38b27316f75345d46e764bc994
ElmiraGhorbani <elmira.ghorbani96@gmail.com>
2023-07-25 15:14:05 +0330

    update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name='chatgpt_long_term_memory',
     version='0.1.0',
     author='Elmira Ghorbani',
-    description='The ChatGPT Long Term Memory package is a powerful tool designed to empower your projects with the ability to handle a large number of simultaneous users. It achieves this by seamlessly integrating an extensive knowledge base and adaptive memory through cutting-edge technologies such as GPT from OpenAI, llama vector index, and Redis datastore. With this comprehensive set of capabilities, you can create highly scalable applications that provide contextually relevant and engaging conversations, enhancing the overall user experience and interaction.',
+    description='The ChatGPT Long Term Memory package is a powerful tool designed to empower your projects with the ability to handle a large number of simultaneous users. ',
     packages=find_packages(),
     install_requires=[
         'redis-chatgpt>=0.1.2',
