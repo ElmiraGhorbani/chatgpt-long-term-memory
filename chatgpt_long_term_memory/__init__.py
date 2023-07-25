74b24563b222d52b44aed81d8033c9c2b1d1ad23
ElmiraGhorbani <elmira.ghorbani96@gmail.com>
2023-07-25 14:34:57 +0330

    update chatgpt_long_term_memory

--- chatgpt_long_term_memory/__init__.py
+++ chatgpt_long_term_memory/__init__.py
@@ -1,11 +1,16 @@
-from chatgpt_long_term_memory.openai_engine import (OpenAIChatBot,
-                                                  OpenAIChatConfig,
-                                                  retry_on_openai_errors)
+from chatgpt_long_term_memory.conversation.chatgpt_chatbot_client import \
+    ChatbotClient
+from chatgpt_long_term_memory.conversation.chatgpt_index_client import \
+    ChatGPTClient
 from chatgpt_long_term_memory.llama_index_helpers import DocIndexer, Retrievers
 from chatgpt_long_term_memory.llama_index_helpers.config import (
     IndexConfig, RetrieversConfig)
 from chatgpt_long_term_memory.memory import ChatMemory
 from chatgpt_long_term_memory.memory.chat_memory import ChatMemoryConfig
+from chatgpt_long_term_memory.openai_engine import (OpenAIChatBot,
+                                                    OpenAIChatConfig,
+                                                    TokenCounterConfig,
+                                                    retry_on_openai_errors)
 
 __all__ = [
     "DocIndexer",
@@ -16,5 +21,8 @@ __all__ = [
     "ChatMemoryConfig",
     "OpenAIChatConfig",
     "OpenAIChatBot",
-    "retry_on_openai_errors"
+    "retry_on_openai_errors",
+    "TokenCounterConfig",
+    "ChatGPTClient",
+    "ChatbotClient"
 ]
